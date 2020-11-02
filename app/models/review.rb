f50b039d9f5e8ec10d2c233d2790b917783b10aa
Nico Proto <nicolasproto@gmail.com>
2020-11-02 11:12:40 +0300

    Updated changes

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,7 @@
 class Review < ApplicationRecord
+  after_create :send_notification
+
+  def send_notification
+    ReviewServices::SlackNotification.new.build_message(title, content, rating).deliver
+  end
 end
